ecc0e0243e2601b655935434a30a0763fa84f0f7
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-11 07:36:23 +0000

    Nested and function callbacks added

--- src/app.js
+++ src/app.js
@@ -11,3 +11,23 @@ setTimeout(()=>{
     console.log("Here is your data from the server")
 },3000)
 console.log("You have reached the end of the file!");
+
+setTimeout(()=>{
+    document.body.style.backgroundColor = 'red';
+},1000)
+setTimeout(()=>{
+    document.body.style.backgroundColor = 'orange';
+},2000)
+setTimeout(()=>{
+    document.body.style.backgroundColor = 'yellow';
+},3000)
+
+const delayedColorChange = (newColor, delay) =>{
+    setTimeout(()=>{
+        document.body.style.backgroundColor = newColor;
+    },delay)
+}
+
+delayedColorChange('blue',1000);
+delayedColorChange('green',2000);
+delayedColorChange('yellow',3000);
