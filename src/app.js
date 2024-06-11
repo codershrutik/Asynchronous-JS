b09854956659f7d69cf1b557d969cebbc5006482
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-11 07:29:53 +0000

    Added JS Single Threading Example

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
-const multiply = (x,y) => x*y;
+// const multiply = (x,y) => x*y;
 
-const square = x => multiply(x,x);
+// const square = x => multiply(x,x);
 
-const isRightTriangle = (a,b,c) =>(
-    square(a) + square(b) === square(c)
-)
+// const isRightTriangle = (a,b,c) =>(
+//     square(a) + square(b) === square(c)
+// )
+
+console.log("Sneidng request to server!")
+setTimeout(()=>{
+    console.log("Here is your data from the server")
+},3000)
+console.log("You have reached the end of the file!");
